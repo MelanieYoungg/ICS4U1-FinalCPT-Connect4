d5ab0a34ed660979b19ef69976d19b2384d85969
MelanieYoungg <melanie.a.y30@gmail.com>
2022-01-18 09:14:20 -0500

    Created Connect Piece object

---
+++ ConnectPiece.java
@@ -0,0 +1,46 @@
+import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+
+public class ConnectPiece{
+	//properties
+	int intX;
+	int intY;
+	int intXpos;
+	int intYpos;
+	int intDefY = 0;
+	String strPlayer1File;
+	String strPlayer2File;
+	String strTurn = "player1";
+	
+	BufferedImage player1piece = null;
+	BufferedImage player2piece = null;
+	
+	//method
+	
+	public void drawIt(Graphics g){
+		/*/if(strTurn.equalsIgnoreCase("player1")){
+			g.drawImage(player1piece, intX-50, intY-50, null);
+		}else if (strTurn.equalsIgnoreCase("player2")){
+			g.drawImage(player2piece, intX-50, intY-50, null);
+		}/*/
+		g.setColor(Color.BLACK);
+		g.fillOval(intX-50, intY-50, 100,100);
+		
+	}
+	
+	//constructor
+	public ConnectPiece(){
+		intXpos = intX;
+		intYpos = intY;
+		try{
+			player1piece = ImageIO.read(new File(System.getProperty("user.dir") + "\\Pictures\\" + strPlayer1File));
+			System.out.println(strPlayer1File);
+			player2piece = ImageIO.read(new File(System.getProperty("user.dir") + "\\Pictures\\" + strPlayer2File));
+		}catch(IOException e){
+			System.out.println("Error loading game piece");
+		}
+	}
+
+}
