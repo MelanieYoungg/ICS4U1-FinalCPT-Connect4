2b45925b265a2f002e46241ad68389a2134f99ae
ethan-rng <68671755+ethan-rng@users.noreply.github.com>
2022-01-27 00:41:38 -0500

    version final
    version final

--- moduleBackendBoard.java
+++ moduleBackendBoard.java
@@ -1,12 +1,44 @@
-import java.util.Scanner;
-
+/**
+ * <h1>moduleBackendBoard.java</h1>
+ * This class allows the controllerMainFrame.java class to
+ * 	- calculate if there is a win in the game connect4,<p>
+ * 	- interface and allow players to add pieces to the column<p>
+ *  - display the state of the board in a system.out.println() for troubleshooting<p>
+ * And uses a 6x7 2D array to hold the game state of the board<p>
+ * This was written specifically for the ICS4U1-FinalCPT-Connect4 Project<p>
+ *
+ * @author  Ethan Rong, Leo Li, Melanie Young
+ * @version 1.0
+ * @since   2022-01-26
+ */
 public class moduleBackendBoard{
 	//PROPERTIES
+	/**
+	 * The board itself, which is made up of a 6x7 int 2D Array
+	 * Holds values of 0, 1, 2.
+	 * 	- 0 is an empty slot
+	 * 	- 1 is a slot taken by player 1
+	 * 	- 2 is a slot taken by player 2
+	 */
 	int intBoard[][] = new int[6][7];
+
+	/**
+	 * The number of terms that have gone by. Used to also determine if it is player 1 or player 2 turn
+	 * by dividing the number of turns by 2 and seeing the remainder (aka modulus)
+	 */
 	int intTurn;
+
+	/**
+	 * Called outside of the program to help navigate the tiles on the frontend to the correct row
+	 */
 	int intCurrentRow;
-		
-	//Add Player
+
+	//Add Position
+	/**
+	 * Will update intBoard[][] based on the column the current player selects to put their piece
+	 * @param intCol the array index (0-5) representing the column the current player wants to put their piece in
+	 * @return Returns true if the piece was successfully added, false if the column is full already
+	 */
 	public boolean addPosition(int intCol){
 		for(int intRow = 5; intRow >= 0; intRow--){
 			int intPiece = intBoard[intRow][intCol];
@@ -24,7 +56,12 @@ public class moduleBackendBoard{
 		return false;
 	}
 		
-	//Check For Winner 
+	//Check For Winner
+	/**
+	 * Scans All 4 Angles to Win (Horizontal, Vertical, Diagonal Up-to-Down and Diagonal Down-to-Up)
+	 * @param intPlayer the player that you want to check a win for (1 or 2)
+	 * @return Returns true if the intPlayer won, false if intPlayer lost
+	 */
 	public boolean checkWinner(int intPlayer ){
 		//Row
 		for(int intRow = 0; intRow < 6; intRow++){
@@ -82,6 +119,10 @@ public class moduleBackendBoard{
 	}
 		
 	//Printout Screen (used for troubleshooting)
+	/**
+	 * Used for troubleshooting to print out the entire board
+	 * @return void
+	 */
 	public void printScreen(){
 		System.out.println("0\t1\t2\t3\t4\t5\t6");
 		System.out.println("------------------------------------------------------------------------------------------");
@@ -95,24 +136,16 @@ public class moduleBackendBoard{
 	}
 		
 	//CONSTRUCTOR
+	/**
+	 * moduleBackendBoard.java Constructor<p>
+	 * Sets up the intBoard to a state of all 0s aka empty
+	 */
 	public moduleBackendBoard(){
 		intTurn = 0;
 		for(int intRow = 0; intRow < 6; intRow++){
 			for(int intCol = 0; intCol < 7; intCol++){
 				intBoard[intRow][intCol] = 0;
 			}
-		}
-	}
-
-	//MAIN METHOD
-	public static void main(String[] args) {
-		moduleBackendBoard board = new moduleBackendBoard();
-		while(!board.checkWinner(board.intTurn%2+1)){
-			System.out.println(board.checkWinner(board.intTurn%2+1));
-			board.printScreen();
-			Scanner scan = new Scanner(System.in);
-			int intCol = scan.nextInt();
-			board.addPosition(intCol);
 		}
 	}
 }
--- out/production/ICS4U1-FinalCPT-Connect4/README.md
+++
@@ -1,2 +0,0 @@
-# ICS4U1-FinalCPT-Connect4
-A recreation of Connect 4 in Java for our ICS4U1 Computer Science Final CPT
